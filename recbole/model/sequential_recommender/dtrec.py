b3c3718eb65cc011977f5af29f5db5ebdfd1bc59
tripiggyo1 <165139925+tripiggyo1@users.noreply.github.com>
2024-03-28 16:37:30 +0800

    First commit

---
+++ recbole/model/sequential_recommender/dtrec.py
@@ -0,0 +1,194 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2020/8/17 19:38
+# @Author : Yujie Lu
+# @Email  : yujielu1998@gmail.com
+
+# UPDATE:
+# @Time   : 2020/8/19, 2020/10/2
+# @Author : Yupeng Hou, Yujie Lu
+# @Email  : houyupeng@ruc.edu.cn, yujielu1998@gmail.com
+
+r"""
+GRU4Rec
+################################################
+
+Reference:
+    Yong Kiam Tan et al. "Improved Recurrent Neural Networks for Session-based Recommendations." in DLRS 2016.
+
+"""
+
+import torch
+from torch import nn
+from torch.nn.init import xavier_uniform_, xavier_normal_
+
+from recbole.model.abstract_recommender import SequentialRecommender
+from recbole.model.loss import BPRLoss
+
+import random
+
+
+class GRU4Rec(SequentialRecommender):
+    r"""GRU4Rec is a model that incorporate RNN for recommendation.
+
+    Note:
+
+        Regarding the innovation of this article,we can only achieve the data augmentation mentioned
+        in the paper and directly output the embedding of the item,
+        in order that the generation method we used is common to other sequential models.
+    """
+
+    def __init__(self, config, dataset):
+        super(GRU4Rec, self).__init__(config, dataset)
+
+        # load parameters info
+        self.embedding_size = config['embedding_size']
+        self.hidden_size = config['hidden_size']
+        self.loss_type = config['loss_type']
+        self.num_layers = config['num_layers']
+        self.dropout_prob = config['dropout_prob']
+
+        self.dataset = config['dataset']
+        self.mask_token = self.n_items
+
+        # define layers and loss
+        self.item_embedding = nn.Embedding(self.n_items + 1, self.embedding_size, padding_idx=0)
+        self.emb_dropout = nn.Dropout(self.dropout_prob)
+        self.gru_layers = nn.GRU(
+            input_size=self.embedding_size,
+            hidden_size=self.hidden_size,
+            num_layers=self.num_layers,
+            bias=False,
+            batch_first=True,
+        )
+        self.dense = nn.Linear(self.hidden_size, self.embedding_size)
+        if self.loss_type == 'BPR':
+            self.loss_fct = BPRLoss()
+        elif self.loss_type == 'CE':
+            self.loss_fct = nn.CrossEntropyLoss()
+        else:
+            raise NotImplementedError("Make sure 'loss_type' in ['BPR', 'CE']!")
+
+        # parameters initialization
+        self.apply(self._init_weights)
+
+        torch.backends.cudnn.enabled = False
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Embedding):
+            xavier_normal_(module.weight)
+        elif isinstance(module, nn.GRU):
+            xavier_uniform_(module.weight_hh_l0)
+            xavier_uniform_(module.weight_ih_l0)
+
+    def forward(self, item_seq, item_seq_len):
+
+        item_seq_emb = self.item_embedding(item_seq)
+        item_seq_emb_dropout = self.emb_dropout(item_seq_emb)
+        # item_seq_emb_dropout = item_seq_emb_dropout.permute(1, 0, 2)
+        gru_output, _ = self.gru_layers(item_seq_emb_dropout)
+
+        gru_output = self.dense(gru_output)
+        # the embedding of the predicted item, shape of (batch_size, embedding_size)
+        seq_output = self.gather_indexes(gru_output, item_seq_len - 1)
+        return seq_output
+
+    def calculate_loss(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        seq_output = self.forward(item_seq, item_seq_len)
+        pos_items = interaction[self.POS_ITEM_ID]
+        if self.loss_type == 'BPR':
+            neg_items = interaction[self.NEG_ITEM_ID]
+            pos_items_emb = self.item_embedding(pos_items)
+            neg_items_emb = self.item_embedding(neg_items)
+            pos_score = torch.sum(seq_output * pos_items_emb, dim=-1)  # [B]
+            neg_score = torch.sum(seq_output * neg_items_emb, dim=-1)  # [B]
+            loss = self.loss_fct(pos_score, neg_score)
+            return loss
+        else:  # self.loss_type = 'CE'
+            test_item_emb = self.item_embedding.weight
+            logits = torch.matmul(seq_output, test_item_emb.transpose(0, 1))
+            loss = self.loss_fct(logits, pos_items)
+            return loss
+
+    def predict(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        test_item = interaction[self.ITEM_ID]
+        seq_output = self.forward(item_seq, item_seq_len)
+        test_item_emb = self.item_embedding(test_item)
+        scores = torch.mul(seq_output, test_item_emb).sum(dim=1)  # [B]
+
+        return scores
+
+    def full_sort_predict(self, interaction):
+        item_seq = interaction[self.ITEM_SEQ]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        seq_output = self.forward(item_seq, item_seq_len)
+        test_items_emb = self.item_embedding.weight
+        scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1))  # [B, n_items]
+
+        return scores
+
+    def customized_sort_predict(self, interaction):
+        item_seq = interaction['item_id_list']
+        type_seq = interaction['item_type_list']
+        truth = interaction['item_id']
+        if self.dataset == "ijcai_beh":
+            raw_candidates = [73, 3050, 22557, 5950, 4391, 6845, 1800, 2261, 13801, 2953, 4164, 32090, 3333, 44733,
+                              7380, 790, 1845, 2886, 2366, 21161, 6512, 1689, 337, 3963, 3108, 715, 169, 2558, 6623,
+                              888, 6708, 3585, 501, 308, 9884, 1405, 5494, 6609, 7433, 25101, 3580, 145, 3462, 5340,
+                              1131, 6681, 7776, 8678, 52852, 19229, 4160, 33753, 4356, 920, 15312, 43106, 16669, 1850,
+                              2855, 43807, 15, 8719, 89, 3220, 36, 2442, 9299, 8189, 701, 300, 526, 4564, 516, 1184,
+                              178, 2834, 16455, 9392, 22037, 344, 15879, 3374, 2984, 3581, 11479, 6927, 779, 5298,
+                              10195, 39739, 663, 9137, 24722, 7004, 7412, 89534, 2670, 100, 6112, 1355]
+        elif self.dataset == "retail_beh":
+            raw_candidates = [101, 11, 14, 493, 163, 593, 1464, 12, 297, 123, 754, 790, 243, 250, 508, 673, 1161, 523,
+                              41, 561, 2126, 196, 1499, 1093, 1138, 1197, 745, 1431, 682, 1567, 440, 1604, 145, 1109,
+                              2146, 209, 2360, 426, 1756, 46, 1906, 520, 3956, 447, 1593, 1119, 894, 2561, 381, 939,
+                              213, 1343, 733, 554, 2389, 1191, 1330, 1264, 2466, 2072, 1024, 2015, 739, 144, 1004, 314,
+                              1868, 3276, 1184, 866, 1020, 2940, 5966, 3805, 221, 11333, 5081, 685, 87, 2458, 415, 669,
+                              1336, 3419, 2758, 2300, 1681, 2876, 2612, 2405, 585, 702, 3876, 1416, 466, 7628, 572,
+                              3385, 220, 772]
+        elif self.dataset == "tmall_beh":
+            raw_candidates = [2544, 7010, 4193, 32270, 22086, 7768, 647, 7968, 26512, 4575, 63971, 2121, 7857, 5134,
+                              416, 1858, 34198, 2146, 778, 12583, 13899, 7652, 4552, 14410, 1272, 21417, 2985, 5358,
+                              36621, 10337, 13065, 1235, 3410, 14180, 5083, 5089, 4240, 10863, 3397, 4818, 58422, 8353,
+                              14315, 14465, 30129, 4752, 5853, 1312, 3890, 6409, 7664, 1025, 16740, 14185, 4535, 670,
+                              17071, 12579, 1469, 853, 775, 12039, 3853, 4307, 5729, 271, 13319, 1548, 449, 2771, 4727,
+                              903, 594, 28184, 126, 27306, 20603, 40630, 907, 5118, 3472, 7012, 10055, 1363, 9086, 5806,
+                              8204, 41711, 10174, 12900, 4435, 35877, 8679, 10369, 2865, 14830, 175, 4434, 11444, 701]
+        customized_candidates = list()
+        for batch_idx in range(item_seq.shape[0]):
+            seen = item_seq[batch_idx].cpu().tolist()
+            cands = raw_candidates.copy()
+            for i in range(len(cands)):
+                if cands[i] in seen:
+                    new_cand = random.randint(1, self.n_items)
+                    while new_cand in seen:
+                        new_cand = random.randint(1, self.n_items)
+                    cands[i] = new_cand
+            cands.insert(0, truth[batch_idx].item())
+            customized_candidates.append(cands)
+        candidates = torch.LongTensor(customized_candidates).to(item_seq.device)
+        item_seq_len = torch.count_nonzero(item_seq, 1)
+        item_seq, type_seq = self.reconstruct_test_data(item_seq, item_seq_len, type_seq)
+
+        seq_output = self.forward(item_seq, item_seq_len)
+        # seq_output = self.gather_indexes(seq_output, item_seq_len)  # [B H]
+        test_items_emb = self.item_embedding(candidates)  # delete masked token
+        scores = torch.bmm(test_items_emb, seq_output.unsqueeze(-1)).squeeze()  # [B, item_num]
+
+        return scores
+
+    def reconstruct_test_data(self, item_seq, item_seq_len, item_type):
+        """
+        Add mask token at the last position according to the lengths of item_seq
+        """
+        padding = torch.zeros(item_seq.size(0), dtype=torch.long, device=item_seq.device)  # [B]
+        item_seq = torch.cat((item_seq, padding.unsqueeze(-1)), dim=-1)  # [B max_len+1]
+        item_type = torch.cat((item_type, padding.unsqueeze(-1)), dim=-1)
+        for batch_id, last_position in enumerate(item_seq_len):
+            item_seq[batch_id][last_position] = self.mask_token
+        return item_seq, item_type
+
